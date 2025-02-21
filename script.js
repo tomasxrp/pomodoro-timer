c14f125011a4098604c9050de82b617bc26a5436
Tomas <rjosetomas6@gmail.com>
2025-02-20 23:22:01 -0300

    New design

--- script.js
+++ script.js
@@ -1,81 +1,97 @@
-let timer;
-let focusTime = 25;
-let focusTimeToSeconds;
-var timerState = new Boolean(false);
-const timerDisplay = document.querySelector('.timer h1');
-const startButton = document.getElementById('btn-start');
-const resetButton = document.getElementById('btn-reset');
+const minuteDisplay = document.getElementById('minutes');
+const secondDisplay = document.getElementById('seconds');
+const startButton = document.querySelector('start');
+const buttonCurrentMode = document.getElementById('currentMode');
 
-const sound = new Audio('./resource/alarma.mp3'); 
-
+let currentMode = 0; // 0 = focusMode, 1 = break
+let state = false; // false = stopped, true = running
+let timerInterval;
+let remainingSeconds;
 
 function startTimer() {
-    if (timerState == false) {
-        timer = setInterval(updateTimer, 1000);
-        timerState = true;
-        startButton.textContent = 'Pausar';
+
+    if (currentMode === 0) {
+        if (state) {
+            clearInterval(timerInterval);
+            state = false;
+        } else {
+            state = true;
+            if (!remainingSeconds) {
+                remainingSeconds = minuteToSeconds(25); // Inicializa con 25 minutos si no hay tiempo restante
+            }
+            startFocusMode(remainingSeconds);
+        }
+    }
+    else if (currentMode === 1) {
+        if (state) {
+            clearInterval(timerInterval);
+            state = false;
+        } else {
+            console.log("state false")
+            state = true;
+            if (!remainingSeconds) {
+                remainingSeconds = minuteToSeconds(5); // Inicializa con 25 minutos si no hay tiempo restante
+            }
+            startFocusMode(remainingSeconds);
+        }
+    }
+}
+
+function startFocusMode(seconds) {
+    updateClock(seconds);
+
+    timerInterval = setInterval(() => {
+        seconds--;
+        remainingSeconds = seconds; // Actualiza el tiempo restante
+        if (seconds < 0) {
+            clearInterval(timerInterval);
+            console.log("Termina acaaaaaa")
+            state = false;
+            nextMode();
+            startTimer();
+            // Aquí puedes agregar lógica para cambiar de modo o notificar al usuario
+        } else {
+            updateClock(seconds);
+        }
+    }, 1000);
+}
+
+function nextMode(){
+    if (currentMode === 0) {
+        currentMode = 1;
+        remainingSeconds = minuteToSeconds(5);
+        updateClock(remainingSeconds);
+        buttonCurrentMode.textContent = 'Break';
+        changeColor();
     } else {
-        clearInterval(timer);
-        timerState = false;
-        startButton.textContent = 'Comenzar';
+        currentMode = 0;
+        remainingSeconds = minuteToSeconds(25);
+        updateClock(remainingSeconds);
+        buttonCurrentMode.textContent = 'Focus';
+        changeColor();
     }
 }
 
-function resetTimer() {
-    clearInterval(timer);
-    timerState = false;
-    startButton.textContent = 'Comenzar';
-    focusTimeToSeconds = minuteToSeconds(focusTime);
-    displayTime(focusTimeToSeconds);
+function updateClock(seconds) {
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
+
+    minuteDisplay.textContent = minutes.toString().padStart(2, '0');
+    secondDisplay.textContent = remainingSeconds.toString().padStart(2, '0');
 }
-
-
-
-function updateTimer() {
-    if (focusTimeToSeconds > 0) {
-        focusTimeToSeconds--;
-        displayTime(focusTimeToSeconds);
-    }
-    else{
-        clearInterval(timer);
-        playSound();
-    }
-    
-}
-
-function playSound() {
-    sound.play();
-}
-
 
 function minuteToSeconds(minutes) {
     return minutes * 60;
 }
 
-function startFocus(){
-    focusTime = 25;
-    focusTimeToSeconds = minuteToSeconds(focusTime);
-
-    displayTime(focusTimeToSeconds);
-}
-
-function startShortBreak(){
-    focusTime = 5;
-    focusTimeToSeconds = 2;
-
-    displayTime(focusTimeToSeconds);
-}
-
-function startLongBreak(){
-    focusTime = 15;
-    focusTimeToSeconds = minuteToSeconds(focusTime);
-
-    displayTime(focusTimeToSeconds);
-}
-
-
-function displayTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = seconds % 60;
-    timerDisplay.textContent = `${minutes < 10 ? '0' : ''}${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+function changeColor(){
+    if(currentMode === 0){
+        document.body.style.backgroundColor = "#F2FFF5";
+        minuteDisplay.style.color = "#14401D";
+        secondDisplay.style.color = "#14401D";
+    }else{
+        document.body.style.backgroundColor = "#FFF2F2";
+        minuteDisplay.style.color = "#471515";
+        secondDisplay.style.color = "#471515";
+    }
 }
