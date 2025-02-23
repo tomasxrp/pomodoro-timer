b0655adc8f39bdf3118496a93578b04a686aba45
Tomas <rjosetomas6@gmail.com>
2025-02-22 22:36:11 -0300

    Refactor button elements and update styles; delete alarm sound

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 const minuteDisplay = document.getElementById('minutes');
 const secondDisplay = document.getElementById('seconds');
-const startButton = document.querySelector('start');
+const startButton = document.getElementById('start-btn');
+const nextModeButton = document.getElementById('nextMode-btn');
+const resetButton = document.getElementById('reset-btn');
 const buttonCurrentMode = document.getElementById('currentMode');
+const imgStartButton = document.getElementById('start-btn-img');
 
 let currentMode = 0; // 0 = focusMode, 1 = break
 let state = false; // false = stopped, true = running
 let timerInterval;
 let remainingSeconds;
 
+const lightModeColors = {
+    bgFocusMode : '#F2FFF5',
+    primaryFocusMode : '#14401D',
+    secundaryFocusMode : '#8CE8A1',
+    thirdFocusMode : '#DAFAE0',
+    timerColorFocusMode : '#14401D',
+    fontColorFocusMode : '#14401D',
+    mainFocusButtonColor : '#296635',
+    secondaryFocusButtonColor : '#289c3f',
+
+    bgBreakMode : '#FFF2F2',
+    primaryBreakMode : '#471515',
+    secundaryBreakMode : '#ff7373',
+    thirdBreakMode : '#fab4b4',
+    timerColorBreakMode : '#471515',
+    fontColorBreakMode : '#fff',
+    mainBreakButtonColor : '#782424',
+    secondaryBreakButtonColor : '#b84f4f',
+}
+
 function startTimer() {
 
     if (currentMode === 0) {
         if (state) {
-            clearInterval(timerInterval);
             state = false;
+            clearInterval(timerInterval);
+            
         } else {
             state = true;
             if (!remainingSeconds) {
@@ -86,12 +110,30 @@ function minuteToSeconds(minutes) {
 
 function changeColor(){
     if(currentMode === 0){
-        document.body.style.backgroundColor = "#F2FFF5";
-        minuteDisplay.style.color = "#14401D";
-        secondDisplay.style.color = "#14401D";
+        document.body.style.backgroundColor = lightModeColors.bgFocusMode;
+        minuteDisplay.style.color = lightModeColors.timerColorFocusMode;
+        secondDisplay.style.color = lightModeColors.timerColorFocusMode;
+        startButton.style.backgroundColor = lightModeColors.mainFocusButtonColor;
+        startButton.style.color = lightModeColors.fontColorFocusMode;
+        nextModeButton.style.backgroundColor = lightModeColors.secondaryFocusButtonColor;
+        nextModeButton.style.color = lightModeColors.fontColorFocusMode;
+        resetButton.style.backgroundColor = lightModeColors.secondaryFocusButtonColor;
+        resetButton.style.color = lightModeColors.fontColorFocusMode;
+        buttonCurrentMode.style.backgroundColor = lightModeColors.thirdFocusMode;
+        buttonCurrentMode.style.color = lightModeColors.fontColorFocusMode;
+        
     }else{
-        document.body.style.backgroundColor = "#FFF2F2";
-        minuteDisplay.style.color = "#471515";
-        secondDisplay.style.color = "#471515";
+        document.body.style.backgroundColor = lightModeColors.bgBreakMode;
+        minuteDisplay.style.color = lightModeColors.timerColorBreakMode;
+        secondDisplay.style.color = lightModeColors.timerColorBreakMode;
+        startButton.style.backgroundColor = lightModeColors.mainBreakButtonColor;
+        startButton.style.color = lightModeColors.fontColorBreakMode;
+        nextModeButton.style.backgroundColor = lightModeColors.secondaryBreakButtonColor;
+        nextModeButton.style.color = lightModeColors.fontColorBreakMode;
+        resetButton.style.backgroundColor = lightModeColors.secondaryBreakButtonColor;
+        resetButton.style.color = lightModeColors.fontColorBreakMode;
+        buttonCurrentMode.style.backgroundColor = lightModeColors.thirdBreakMode;
+        buttonCurrentMode.style.color = lightModeColors.fontColorBreakMode;
     }
 }
+
